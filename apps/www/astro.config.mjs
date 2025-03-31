5fcf640be6e085564c4c18a059f885647a68b656
vasyaqwe <elekton.fafik@gmail.com>
2025-03-31 23:12:18 +0300

    upd base url

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-   site: "https://vasyldev.cc",
+   site: "https://vasyaqwe.com",
    integrations: [sitemap()],
    experimental: {
       svg: true,

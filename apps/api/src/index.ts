151003665a5210ff9bb0518638ee944b7c134475
vasyaqwe <elekton.fafik@gmail.com>
2025-08-19 00:21:52 +0300

    upd api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,24 @@
-import app from "./app"
+// import app from "./app"
 
-const server = Bun.serve({
-   port: 8080,
-   hostname: "0.0.0.0",
-   fetch: app.fetch,
+// const server = Bun.serve({
+//    port: 8080,
+//    hostname: "0.0.0.0",
+//    fetch: app.fetch,
+// })
+
+// console.log("server running", server.port)
+
+import { Hono } from "hono"
+import { invoiceRouter } from "./invoice/router"
+
+const app = new Hono()
+
+app.get("/hello", (c) => {
+   return c.json({
+      message: "Hello from Hono!",
+   })
 })
 
-console.log("server running", server.port)
+app.route("/invoice", invoiceRouter)
+
+export default app

eff6fa7062b19a2c94214e4eef8bc15e09bc4971
vasyaqwe <elekton.fafik@gmail.com>
2025-08-19 09:56:02 +0300

    upd api

--- apps/api/src/invoice/pdf.ts
+++ apps/api/src/invoice/pdf.ts
@@ -1,7 +1,7 @@
-import { renderToStream } from "@react-pdf/renderer"
 import { InvoicePdf } from "./invoice"
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export async function generatePdf(props: any) {
+   const { renderToStream } = await import("@react-pdf/renderer")
    return renderToStream(InvoicePdf(props))
 }

76445c2a32cb59eca6c77ede167cdf53890a2a35
vasyaqwe <elekton.fafik@gmail.com>
2025-11-11 15:54:30 +0200

    add blog description

--- apps/www/src/content.config.ts
+++ apps/www/src/content.config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
    schema: () =>
       z.object({
          title: z.string(),
+         description: z.string(),
          date: z.string(),
          draft: z.boolean(),
       }),
